36d26a821f7ff6ced011aba05de1d45c56f208d4
Adam Tretera <adam.tretera@ackee.cz>
2023-05-21 14:20:26 +0200

    delete tests

--- calculator/test_sum.py
+++ calculator/test_sum.py
@@ -3,32 +3,6 @@ from unittest import TestCase
 from sum import Calculator
 
 
-class CalculatorTest(TestCase):
-    def setUp(self):
-        self.calculator = Calculator()
-
-
-
-    def test_add(self):
-        result = self.calculator.add(2, 3)
-        self.assertEqual(result, 5)
-
-    def test_subtract(self):
-        result = self.calculator.subtract(5, 3)
-        self.assertEqual(result, 2)
-
-    def test_multiply(self):
-        result = self.calculator.multiply(2, 123)
-        self.assertEqual(result, 6)
-
-    def test_divide(self):
-        result = self.calculator.divide(10, 2)
-        self.assertEqual(result, 5)
-
-    def test_divide_by_zero(self):
-        with self.assertRaises(ValueError):
-            self.calculator.divide(10, 0)
-
 
 if __name__ == '__main__':
     TestCase.main()
